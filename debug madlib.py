b1fca49ad8579ac9597ff371fd5389dbdce7a6f3
blfisher1 <blfisher1@sbcglobal.net>
2016-04-03 13:57:11 -0500

    Added files via upload

---
+++ debug madlib.py
@@ -0,0 +1,56 @@
+#initialize the variables
+
+pluralNoun1 = " "
+adjective1 = " "
+adjective2 = " "
+bodyPartPlural = " "
+adjective3 = " "
+animalPlural1 = " "
+animalPlural2 = " "
+nounPlural1 = " "
+celebrity = " "
+noun1 = " "
+bird = " "
+noun2 = " "
+noun3 = " "
+liquid = " "
+nounPlural1 = " "
+noun4 = " "
+somethingGreasy = " "
+somethingHealthy = " "
+adjective3 = " "
+
+#Ask the user to specifiy values for the variables
+pluralNoun1 =input("Enter a noun: ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+bodyPartPlural = input("Enter a plural body part: ")
+adjective3 = input("Enter an adjective: ")
+animalPlural1 = input("Enter the plural name of an animal: ")
+animalPlural2 = input("Enter the plural name of another animal: ")
+nounPlural1 = input("Enter a plural noun: ")
+celebrity = input("Enter the name of a celebrity: ")
+noun1 = input("Enter a noun: ")
+bird = input("Enter the name of a bird: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a noun: ")
+nounPlural1 = input("Enter a plural noun: ")
+noun4 = input("Enter a noun: ")
+somethingGreasy = input("Enter something greasy: ")
+somethingHealthy = input("Enter something healthy: ")
+adjective3 = input("Enter an adjective: ")
+
+#Display the story
+#Don't forget to format the strings when they are displayed.
+print("Inspectors from the State Department of Health and " + pluralNoun1 + " came here today to inspect the " + adjective1 + " kitchen and to make sure that our " + adjective2 + " cooks were washing their " + bodyPartPlural + " before preparing our " + adjective3 + " meals.")
+print("And that there are no little " + animalPlural1 + " or " + animalPlural2 + " running around in the kitchen and spreading " + nounPlural1 + ". ")
+print("They checked the lunch prepared by our dietician, " + celebrity + ". ")
+print("We had spaghetti and"  + noun1 + " balls. ")
+print("On Tuesdays, we have boiled " + bird + " with rice. ")
+print("On Wednesdays, we have a choice of " + noun2 + " soup or a " + noun3 + " omlette with chocolate milk sauce. ")
+print("The inspector found a lot of " + nounPlural1 + " in the salad and said there was too much " + noun4 + " in the milk. ")
+print("In the future, we will have less " + somethingGreasy + " to eat and more " + somethingHealthy + ". ")
+print("But I bet it will still taste " + adjective3 + " .")
+
+
+
